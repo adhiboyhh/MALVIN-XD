3491d27b73d6ef1c054dadd1149467d6d89938d1
adhiboyhh <adhithyanks531@gmail.com>
2025-07-26 13:52:47 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~s6MFhaIY#veqlIH5t6pB8Whdy5dHb1J2HaPeWMjtizHnw49VAYpc",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "+916235161494", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
